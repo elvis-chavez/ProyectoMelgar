9e61024b687d1a5c03315875f8ef68e4f65fad47
Jose <jeduramos7001@gmail.com>
2025-04-28 23:01:17 -0600

    Agregar Controller.php con la clase base controladora

---
+++ src/lib/Controller.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Jose\MultiserviciosMelgar\lib;
+
+use Jose\MultiserviciosMelgar\lib\View;
+
+/**
+ * Clase Controller
+ * Controlador base de la aplicación
+ */
+
+class Controller {
+    private View $view;
+
+    public function __construct() {
+        $this->view = new View();
+    }
+    
+    public function render(string $name, array $data = []) {
+        $this->view->render($name, $data);
+    }
+    
+    protected function get(string $param) {
+        if (!isset($_GET[$param])) {
+            return NULL;
+        }
+        return $_GET[$param];
+    }
+
+    protected function post(string $param) {
+        if (!isset($_POST[$param])) {
+            return NULL;
+        }
+        return $_POST[$param];
+    }
+}
