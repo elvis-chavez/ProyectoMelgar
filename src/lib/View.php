cfee207b973396f317f7215bf25d8c85db6b34d4
Jose <jeduramos7001@gmail.com>
2025-04-28 22:57:17 -0600

    Agregar View.php con la funcion de renderizar las vistas

---
+++ src/lib/View.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Jose\MultiserviciosMelgar\lib;
+
+/**
+ * Clase View
+ * Renderiza las vistas de la aplicación
+ */
+
+class View {
+    private $d;
+    
+    public function render(string $name, array $data = []) {
+        $this->d = $data;
+        require 'src/views/' . $name . '.php';
+    }
+}
