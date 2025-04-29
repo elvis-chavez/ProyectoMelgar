d6cdc83361b3c81e17e92cd30dc0701cdb80abd6
Jose <jeduramos7001@gmail.com>
2025-04-28 23:03:37 -0600

    Agregar controlador LoginController.php

---
+++ src/controllers/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Jose\MultiserviciosMelgar\controllers;
+
+use Jose\MultiserviciosMelgar\lib\Controller;
+use Jose\MultiserviciosMelgar\models\User;
+
+class LoginController extends Controller {
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function auth() {
+        $username = $this->post('username');
+        $password = $this->post('password');
+
+        if (!is_null($username) && !is_null($password)) {
+            // Verificamos si el usuario existe en la base de datos
+            if (User::exists($username)) {
+                // Verificamos la contraseña
+                $user = User::get($username);
+
+                // Comparamos el password ingresado con el password almacenado en la base de datos
+                if ($user->comparePassword($password)) {
+                    $_SESSION['user'] = serialize($user);
+                    // Redirigimos al usuario a la página de inicio
+                    error_log('Usuario autenticado');
+                    header('Location: /multiservicios-melgar2/home');
+                } else {
+                    // Si las credenciales son incorrectas, redirigimos al login
+                    error_log('Credenciales incorrectas');
+                    header('Location: /multiservicios-melgar2/login');
+                }
+            } else {
+                // Usuario no encontrado
+                error_log('Usuario no encontrado');
+                header('Location: /multiservicios-melgar2/login');
+            }
+        } else {
+            error_log('Datos de login incompletos');
+            $this->render('errors/index');
+        }
+    }
+}
