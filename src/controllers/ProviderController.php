ba5265035c93d736aa538da32ec216e387cc4e70
Jose <jeduramos7001@gmail.com>
2025-05-10 19:39:14 -0600

    Actualizar ProviderController.php: Cambiar namespaces

--- src/controllers/ProviderController.php
+++ src/controllers/ProviderController.php
@@ -1,12 +1,19 @@
 <?php
 
-namespace Jose\MultiserviciosMelgar\controllers;
-use Jose\MultiserviciosMelgar\lib\Controller;
-use Jose\MultiserviciosMelgar\models\User;
+namespace Jose\ProyectoMelgar\controllers;
+
+use Jose\ProyectoMelgar\lib\Controller;
+use Jose\ProyectoMelgar\models\User;
+use Jose\ProyectoMelgar\models\Provider;
 
 class ProviderController extends Controller {
     public function __construct(private User $user)
     {
         parent::__construct();
+    }
+
+    public function getProviders() {
+        $providers = new Provider();
+        $this->render('provider/index', ['user' => $this->user, 'providers' => $providers]);
     }
 }
