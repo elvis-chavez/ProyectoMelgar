03f88730a5bfe913fd99b8c4536c78edc979b222
Jose <jeduramos7001@gmail.com>
2025-05-10 20:49:13 -0600

    Agregar componente base menu.php

---
+++ src/components/menu.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/ProyectoMelgar/home">Multiservicios Melgar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/instagram/home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/instagram/profile">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/instagram/logout">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
