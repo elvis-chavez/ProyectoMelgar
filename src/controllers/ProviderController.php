5c2c4d56cd61977d79b115ac1312abbfd188055c
Jose <jeduramos7001@gmail.com>
2025-05-11 16:48:44 -0600

    Actualizar ProviderController.php: Agregar metodos para insertar, actualizar y buscar por id

--- src/controllers/ProviderController.php
+++ src/controllers/ProviderController.php
@@ -16,4 +16,64 @@ class ProviderController extends Controller {
         $providers = new Provider();
         $this->render('provider/index', ['user' => $this->user, 'providers' => $providers->getAll()]);
     }
+
+    public function getForm() {
+        $this->render('provider/form', ['user' => $this->user]);
+    }
+
+    public function createProvider() {
+        $name = $this->post('name');
+        $contact = $this->post('contact');
+
+        if (
+            !is_null($name) &&
+            !is_null($contact)
+            ) {
+            $provider = new Provider();
+            $provider->setName($name);
+            $provider->setContact($contact);
+
+            $provider->save();
+            header('Location: /ProyectoMelgar/provider');
+        } else {
+            $this->render('errors/index');
+        }
+    }
+
+    public function editForm($id) {
+        if (!is_null($id)) {
+            $provider = Provider::getById($id);
+            
+            if ($provider) {
+                $this->render('provider/form', [
+                    'user' => $this->user,
+                    'provider' => $provider
+                ]);
+            }
+        }
+        $this->render('errors/index');
+    }
+
+    public function updateProvider() {
+        $id = $this->post('id');
+        $name = $this->post('name');
+        $contact = $this->post('contact');
+
+        if (
+            !is_null($id) &&
+            !is_null($name) &&
+            !is_null($contact)
+            ) {
+            $provider = Provider::getById($id);
+            
+            if ($provider) {
+                $provider->setName($name);
+                $provider->setContact($contact);
+                $provider->update($provider);
+
+                header('Location: /ProyectoMelgar/provider');
+            }
+        }
+        $this->render('errors/index');
+    }
 }
