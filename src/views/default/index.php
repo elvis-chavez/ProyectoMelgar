170c269e0a7b431310d684f40d72e2ca43764be7
Jose <jeduramos7001@gmail.com>
2025-05-12 20:00:42 -0600

    Agregar vista base default/index.php

---
+++ src/views/default/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container w-25 mt-5">
+        <h1>Bienvenido a Multiservicios Melgar</h1>
+        <h2>¿Qué deseas hacer?</h2>
+        
+        <div class="mb-3">
+            <a href="/ProyectoMelgar/login" class="btn btn-primary">¿Ya tienes una cuenta? Entra aquí</a>
+        </div>
+        <div class="mb-3">
+            <a href="/ProyectoMelgar/signup" class="btn btn--primary">¿Todavía no tienes una cuenta? Crea una nueva aquí</a>
+        </div>
+    </div>
+</body>
+</html>
