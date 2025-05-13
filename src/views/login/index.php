bce19cec7a3bb5ec22c4e53318d5a56dd6a799f7
Jose <jeduramos7001@gmail.com>
2025-05-12 19:45:58 -0600

    Actualizar vista login/index.php: Agregar bootstrap al formulario de inicio de sesion

--- src/views/login/index.php
+++ src/views/login/index.php
@@ -4,14 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Login</h1>
-    <form action="/ProyectoMelgar/auth" method="POST">
-        <input type="text" name="username" placeholder="Username" required>
-        <input type="text" name="password" placeholder="Password" required>
+    <div class="container w-25 mt-5">
+        <h2>Login</h2>
+        
+        <form action="/ProyectoMelgar/auth" method="POST">
+            <div class="mb-3">
+                <label class="form-label">Usuario</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Nombre de Usuario">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Contraseña del Usuario">
+            </div>
+            <div class="mb-3">
+                <a href="/ProyectoMelgar/signup" class="link-primary">¿Todavía no tienes una cuenta? Crea una nueva aquí</a>
+            </div>
+            <button type="submit" class="btn btn-primary">Ingresar</button>
+        </form>
+    </div>
 
-        <input type="submit" value="Ingresar">
-    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
