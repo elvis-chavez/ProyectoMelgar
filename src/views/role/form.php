9186fef7e076f157b945e571a4e19673594f962d
Jose <jeduramos7001@gmail.com>
2025-05-13 10:24:52 -0600

    Agregar la vista base role/form.php

---
+++ src/views/role/form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Role Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+<body>
+    <?php require_once 'src/components/menu.php' ?>
+
+    <h1>Usuario <?php echo $this->d['user']->getUsername(); ?></h1>
+    <h2>Formulario de Registro de Roles</h2>
+
+    <?php
+        $user = $this->d['user'];
+    ?>
+
+    <?php require_once 'src/components/form_role.php' ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</body>
+</html>
